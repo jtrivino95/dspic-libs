2fe8572b6afd88a8732fc2e3cb47446b2e76e02b
Javier Trivino <Javier.Trivino@trivago.com>
2017-11-11 11:28:24 +0100

    Añadido comentario

--- timer.h
+++ timer.h
@@ -2,6 +2,7 @@
  * Configura el timer de modo que geneŕará
  * una interrupción cada 1/$freq segundos cuando
  * esté activado.
+ * $freq no puede superar el valor de 460800 Hz
  */
 void timerConfig(float freq);
 
